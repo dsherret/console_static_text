28b85abd6bdb958b0ad302f2937e2b7066217216
Ian Bull <irbull@eclipsesource.com>
2023-10-30 13:38:05 -0700

    Add a CI job to Console Static Text
    Adds a GitHub Actions job to run the build & tests on every push. The
    project is built with all feature flag enabled.
    
    This change-set also adds a variety of test cases for the tokenizer in
    `words.rs`. To make testing easier, the `PartialEq` and `Debug` debug
    traits were added to the `WordToken` enum.

--- src/word.rs
+++ src/word.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq, Debug)]
 pub enum WordToken<'a> {
   Word(&'a str),
   WhiteSpace(char),
@@ -29,4 +30,101 @@ pub fn tokenize_words(text: &str) -> Vec<WordToken> {
     tokens.push(WordToken::Word(new_word_text));
   }
   tokens
+}
+
+#[cfg(test)]
+mod tokenize_tests {
+  use super::*;
+
+  #[test]
+  fn tokenize_words_2_words() {
+    let result = tokenize_words("hello world");
+    assert_eq!(
+      result,
+      vec![
+        WordToken::Word("hello"),
+        WordToken::WhiteSpace(' '),
+        WordToken::Word("world")
+      ]
+    );
+  }
+
+  #[test]
+  fn tokenize_words_newline() {
+    let result = tokenize_words("hello\nworld");
+    assert_eq!(
+      result,
+      vec![
+        WordToken::Word("hello"),
+        WordToken::NewLine,
+        WordToken::Word("world")
+      ]
+    );
+  }
+
+  #[test]
+  fn tokenize_words_newline_spaces() {
+    let result = tokenize_words("hello \n world");
+    assert_eq!(
+      result,
+      vec![
+        WordToken::Word("hello"),
+        WordToken::WhiteSpace(' '),
+        WordToken::NewLine,
+        WordToken::WhiteSpace(' '),
+        WordToken::Word("world")
+      ]
+    );
+  }
+
+  #[test]
+  fn tokenize_words_tab_char() {
+    let result = tokenize_words("hello\tworld");
+    assert_eq!(
+      result,
+      vec![
+        WordToken::Word("hello"),
+        WordToken::WhiteSpace('\t'),
+        WordToken::Word("world")
+      ]
+    );
+  }
+
+  #[test]
+  fn tokenize_words_single_word() {
+    let result = tokenize_words("hello");
+    assert_eq!(result, vec![WordToken::Word("hello"),]);
+  }
+
+  #[test]
+  fn tokenize_words_leading_trailing_whitespace() {
+    let result = tokenize_words(" hello ");
+    assert_eq!(
+      result,
+      vec![
+        WordToken::WhiteSpace(' '),
+        WordToken::Word("hello"),
+        WordToken::WhiteSpace(' ')
+      ]
+    );
+  }
+
+  #[test]
+  fn tokenize_words_with_rune_character() {
+    let result = tokenize_words("hello⌘ ⌘world");
+    assert_eq!(
+      result,
+      vec![
+        WordToken::Word("hello⌘"),
+        WordToken::WhiteSpace(' '),
+        WordToken::Word("⌘world")
+      ]
+    );
+  }
+
+  #[test]
+  fn tokenize_words_single_rune_character() {
+    let result = tokenize_words("⌘");
+    assert_eq!(result, vec![WordToken::Word("⌘"),]);
+  }
 }
